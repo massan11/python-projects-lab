46ec65d42146734d9c3c5fecaed84d30aa0df699
Mohammad H Kazemi <mhassan.zoho@gmail.com>
2025-02-14 08:54:28 +0330

    Add docstrings to functions for improved documentation and clarity

--- fundamental_challenge/fundamental_challenge.py
+++ fundamental_challenge/fundamental_challenge.py
@@ -1,6 +1,6 @@
 
 def get_numbers() -> list[int]:
-    
+    """Get a list of numbers from user input."""   
     while True:
         try:
             input_lst = input("Enter numbers sparated by spase: ")
@@ -10,9 +10,11 @@ def get_numbers() -> list[int]:
             print("numbers only!")
             
 def process_numbers(numbers: list[int]) -> list[int]:
+    """Remove duplicates and sort numbers in descending order."""
     return sorted(list(set(numbers)), reverse = True)
 
 def save_to_file(numbers: list[int], filename: str = "output.txt") -> None:
+    """Save numbers to a file."""
     try:
         with open(filename, "w") as f:
             f.write(" ".join(map(str, numbers)))
